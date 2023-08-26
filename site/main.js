a6bc71ac28161dfcf20613ce0e5a0871ab4668e6
Daniil Petrov <waddlespinespig@gmail.com>
2023-08-26 17:54:39 -0400

    Dispose game after reset

--- site/main.js
+++ site/main.js
@@ -69,6 +69,9 @@ let refreshBoard;
 let clickSquare;
 
 function resetGame(){
+    if(game){
+        chessweb.disposeGame(game);
+    }
     game = chessweb.newGame();
     rules = chessweb.getGameRules(game);
 
